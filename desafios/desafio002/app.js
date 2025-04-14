4038472330e36792cbc52ef47274566ec53c71c2
italoesilva <italoliveira95@hotmail.com>
2025-04-14 18:02:31 -0300

    desafio002

---
+++ desafios/desafio002/app.js
@@ -0,0 +1,62 @@
+console.log('Bem vindo ao meu mundo');
+let nome = 'Italo oLIVEIRA';
+console.log(`Olá, ${nome}!`);
+
+
+let linguagemPreferida = prompt('Qual a linguagem de programação que você mais gosta?');
+console.log(linguagemPreferida);
+
+let valor1= 42;
+let valor2= 8;
+let resultado: valor1 + valor2;
+console.log('A soma de ${valor1} e ${valor2} é igual a ${resultado}. ')
+
+
+let valor1 = 10;
+let valor2= 8;
+let resultado = valor1 - valor2;
+console.log('A diferença entre ${valor1} e ${valor2} é igual a ${resultado}');
+
+
+let idade = promp('Digite sua idade');
+if (idade > 17){
+    console.log('Vc é maior de idade');
+} else{
+    console.log('Você é menor de idade');
+}
+
+
+var numero = parseFloat(prompt("Digite um número"));
+
+if(numero > 0){
+    console.log("O número é positivo.");
+} else if (numero < 0){
+    console.log("O numero é negativo");
+} else{
+    console.log("O número é zero");
+}
+
+let numero = 1;
+while (numero <=10){
+    console.log(numero);
+    numero++;
+
+}
+
+let nota = 8;  // substitua pelo valor da nota que deseja testar
+
+if (nota => 7){
+    console.log("Aprobado")
+} else{
+    console.log("Reprovado!");
+}
+
+let numeroInteiroAleatorio = parseInt = Math.random();
+console.log(NumeroAleatório);
+
+let numeroInteiroAleatorio = parseInt(Math.random() * 10) + 1 ;
+console.log(numeroInteiroAleatorio);
+
+let numeroInteiroAleatorio = parseInt(Math.random() * 1000) + 1;
+console.log(numeroInteiroAleatorio);
+    
