1a40ad525b870eedbc75ea1c0c2b98b18e851f03
italoesilva <italoliveira95@hotmail.com>
2025-04-14 17:33:13 -0300

    numeros aleatorios paseInt(Math.random()

--- exercicios/ex001/app.js
+++ exercicios/ex001/app.js
@@ -1,9 +1,20 @@
-alert('Boas vindas ao jogo do número screto');
-let numeroSecreto = 29;
+alert('Boas vindas ao jogo do número secreto');
+
+let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um numero entre 1 e 10');
 
 
-if(chute == numeroSecreto) {
-    console.log('Isso ai! você descobriu o número secreto (5) ');
+// se chute for igual ao número secreto
 
+if (chute == numeroSecreto) {
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
+    
+} else {
+  if (chute > numeroSecreto){
+    alert(`O numero secreto é menor que ${chute}`);
+  } else { 
+  alert(`O número secreto é maior que ${chute}`);
+
+  }
 }
