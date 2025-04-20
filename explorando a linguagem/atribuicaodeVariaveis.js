937ecd4aac8e31b2fdfc0c9766d42490c8b175f3
italoesilva <italoliveira95@hotmail.com>
2025-04-20 17:02:01 -0300

    exercicios java script - explorando a linguagem

---
+++ explorando a linguagem/atribuicaodeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atrubuiçãi de variáveis");
+
+const idade = 29;
+let primeiroNome = "Italo";
+const sobrenome = "Oliveira";
+
+// console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome} `);
+
+let contador = 0
+contador = contador + 1
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(primeiroNomeome);
+
---
+++ explorandoalinguagem/2-trabalhandoComVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com variáveis");
+
+//JS é case sensitve 
+
+
+const idade = 29;
+const nome = "Italo";
+let ano = 2020;
+
+
+
+//não fazer
+//mês = "Janeiro"
