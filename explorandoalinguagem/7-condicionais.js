de601b48a75c7a8f6c06590f4cacf656f4178f4c
italoesilva <italoliveira95@hotmail.com>
2025-04-22 20:58:35 -0300

    fiz ifs encadeados 7-condicionais

---
+++ explorandoalinguagem/7-condicionais.js
@@ -0,0 +1,38 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+
+
+);
+
+const idadeComprador = 17;
+const estaAcompanhada = true;
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+
+if (idadeComprador >= 18) {
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1, 1); //removendo um item
+} else if (estaAcompanhada == true) {
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1, 1); //removendo um item
+
+} else {
+    console.log("Não é maior de idade e não posso vender");
+}
+
+
+
+
+
+// console.log(listaDeDestinos);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
+
+
