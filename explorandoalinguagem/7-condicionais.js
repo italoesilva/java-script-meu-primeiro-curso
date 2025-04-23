d40c0b0c6c873e8e7048a67464af9cbb99d60c84
italoesilva <italoliveira95@hotmail.com>
2025-04-23 20:04:05 -0300

    aprendendo if encadeados

--- explorandoalinguagem/7-condicionais.js
+++ explorandoalinguagem/7-condicionais.js
@@ -7,23 +7,39 @@ const listaDeDestinos = new Array(
 
 );
 
-const idadeComprador = 17;
-const estaAcompanhada = true;
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1); //removendo um item
-} else if (estaAcompanhada == true) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1, 1); //removendo um item
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem");
+    listaDeDestinos.splice(2, 1); //removendo um item
 
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
+console.log("Embarque: \n\n");
+if(idadeComprador >=18 && temPassagemComprada){
+    console.log("Boa viagem");
+}else{ 
+    console.log("Você não pode embarcar");
+}
 
+console.log(listaDeDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1); //removendo um item
+// } else if (estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removendo um item
+
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
 
 
 
